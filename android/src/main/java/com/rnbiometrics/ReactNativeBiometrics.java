b9d648879046efc11d41f967c0bdd23320c96259
Roi Berlin <roibe@wix.com>
2024-12-05 15:13:54 +0200

    request permissions in case missing

--- android/src/main/java/com/rnbiometrics/ReactNativeBiometrics.java
+++ android/src/main/java/com/rnbiometrics/ReactNativeBiometrics.java
@@ -73,6 +73,10 @@ public class ReactNativeBiometrics extends ReactContextBaseJavaModule {
                             resultMap.putString("error", "BIOMETRIC_ERROR_HW_UNAVAILABLE");
                             break;
                         case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                            val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                                putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED, getAllowedAuthenticators(allowDeviceCredentials))
+                            }
+                            startActivityForResult(enrollIntent, REQUEST_CODE)
                             resultMap.putString("error", "BIOMETRIC_ERROR_NONE_ENROLLED");
                             break;
                         case BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED:
